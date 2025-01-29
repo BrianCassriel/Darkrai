b35cf2084d8bb82c07d4cdc4a37669630e7ab008
D1RF <116754323+D1RF@users.noreply.github.com>
2025-01-29 13:37:05 -0800

    Simulation and Simulation_Test

---
+++ Cpsc370Final.Tests/Simulation_Test.cs
@@ -0,0 +1,12 @@
+namespace Cpsc370Final.Tests;
+
+public class SimulationsTest
+{
+    [Fact]
+    public void SimulationTest1()
+    {
+        Assert.Equal((Fireworks.Count + 1), (Fireworks.Add(new Firework())).Count);
+        Assert.Equal((Fireworks.Count - 1), Fireworks.RemoveFirework(0).Count);
+        Assert.Equal(GetFirework(0), Fireworks[0]);
+    }
+}
---
+++ Cpsc370Final/Simulation.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+
+namespace Cpsc370Final;
+
+public class Simulation
+{
+    private List<Firework> Fireworks = new List<Firework>();
+
+    private void AddFirework(Firework NewFirework)
+    {
+        Fireworks.Add(NewFirework);
+    }
+
+    private void RemoveFirework(int FireworkIndex)
+    {
+        Fireworks.RemoveAt(FireworkIndex);
+    }
+
+    private void UpdateAll()
+    {
+        foreach (var firework in Fireworks)
+        {
+            firework.ManageFirework();
+        }
+    }
+
+    private Firework GetFirework(int FireworkIndex)
+    {
+        return Fireworks[FireworkIndex];
+    }
+}
