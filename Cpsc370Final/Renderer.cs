ed5a2988d9ddb99b4fe9391ade0a24538d55295f
BrianCassriel <bccassriel@gmail.com>
2025-01-29 13:42:34 -0800

    Renderer class methods

--- Cpsc370Final/Renderer.cs
+++ Cpsc370Final/Renderer.cs
@@ -1,30 +1,74 @@
+using System.Drawing;
+
 namespace Cpsc370Final;
 
-public class Renderer
+using Simulation;
+using Firework;
+using Color;
+using Pixel;
+
+public static class Renderer
 {
-    private int width;
-    private int height;
-    private int[,] screenBuffer;
-    
-    public Renderer()
+    private static int width;
+    private static int height;
+    private static char[,] screenBuffer;
+    private static int frameTime = 1000 / 24;
+    private static bool shouldExit = false;
+
+    static Renderer()
     {
-        this.SetupConsoleScreenSize();
-        this.SetupScreenBuffer();
+        SetupConsoleScreenSize();
+        SetupScreenBuffer();
+
+        while (shouldExit == false)
+        {
+            Render();
+            Thread.Sleep(frameTime);
+        }
     }
 
-    private void SetupConsoleScreenSize()
+    public static void Draw(Simulation simulation)
     {
-        this.width = Console.WindowWidth;
-        this.height = Console.WindowHeight;
+        Firework[] fireworks = Simulation.getFireworks();
+        for (Firework firework : fireworks)
+        {
+            Position position = firework.getPosition();
+            screenBuffer[position.x, position.y] = new Pixel(firework.getSymbol(), firework.getColor());
+        }
     }
 
-    private void SetupScreenBuffer()
+    public static void Exit()
     {
-        // 2D array to hold the screen buffer
-        this.screenBuffer = new int[this.height, this.width];
+        shouldExit = true;
     }
 
-    private void writeLine(int lineNum, string line)
+    private static void SetupConsoleScreenSize()
+    {
+        width = Console.WindowWidth;
+        height = Console.WindowHeight;
+    }
+
+    private static void SetupScreenBuffer()
+    {
+        screenBuffer = new char[height, width];
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                screenBuffer[i, j] = ' ';
+            }
+        }
+    }
+
+    private static void Render()
+    {
+        for (int i = 0; i < height; i++)
+        {
+            writeLine(i, new string(screenBuffer[i, 0], width));
+        }
+    }
+
+    private static void writeLine(int lineNum, string line)
     {
         Console.SetCursorPosition(0, lineNum);
         Console.Write(line);
