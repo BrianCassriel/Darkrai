c0102f669d08b5a84c61a0840a22123253f50a65
hemi-shah <heshah@chapman.edu>
2025-01-30 13:29:17 -0800

    Fireworks are Placed Randomly

--- Cpsc370Final/Firework.cs
+++ Cpsc370Final/Firework.cs
@@ -13,6 +13,7 @@ namespace Cpsc370Final
         public char centerParticleSymbol { get; } = '*';
         public List<Particle> particles = new List<Particle>();
         public Color particleColor { get; set; }
+        private Random random = new Random();
 
         public Firework()
         {
@@ -35,12 +36,14 @@ namespace Cpsc370Final
         
         private void PlaceParticle(Position particlePos, char particleSymb)
         {
-            if (particlePos.x >= 0 && particlePos.x < Renderer.GetWidth()  && particlePos.y >= 0 && particlePos.y < Renderer.GetHeight())
+            if (particlePos.x >= 0 && particlePos.x < Renderer.GetWidth()  
+                                   && particlePos.y >= 0 && particlePos.y < Renderer.GetHeight())
             {
+                //Renderer.SetPixel(particlePos.x, particlePos.y, particleSymb, particleColor); // this is the line to actually use particles
                 Renderer.SetPixel(FireworkPosition.x, FireworkPosition.y, centerParticleSymbol, particleColor);
             }
         }
-
+        
         public void ManageFirework()
         {
             if (isExploded)
@@ -52,41 +55,54 @@ namespace Cpsc370Final
         public void Launch()
         {
             int startY = Renderer.GetHeight() - 1;
-    
-            int currentY = startY;
-            while (currentY > 5) 
+            int fireworkY = startY;
+            int fireworkX = FireworkPosition.x;
+
+            while (fireworkY > 5) 
             {
-                //Renderer.Clear();
-                Renderer.SetPixel(Renderer.GetWidth() / 2, currentY, '|', particleColor);  
+                Renderer.SetPixel(fireworkX, fireworkY, '|', particleColor);  
                 Thread.Sleep(80);  
-                
-                Renderer.SetPixel(Renderer.GetWidth() / 2, currentY + 1, ' ', particleColor);
-                currentY--;  
+
+                Renderer.SetPixel(fireworkX, fireworkY + 1, ' ', particleColor);
+                fireworkY--;  
             }
 
-            FireworkPosition.y = currentY; 
+            FireworkPosition.y = fireworkY; 
             isExploded = true;
             CreateParticles(); 
+        }
+ 
+
+        private void UpdateCenterPosition()
+        {
+            int maxY = Renderer.GetHeight() / 2;
+            int minY = 5; 
+        
+            FireworkPosition.x = random.Next(0, Renderer.GetWidth());
+            FireworkPosition.y = random.Next(minY, maxY);
         }
 
         public void OnFrame()
         {
             if (!isExploded)
             {
-                Launch();  
-                FireworkPosition.y -= 1; // Move up by 1
-            
+                Launch();
+                FireworkPosition.y -= 1;
+
                 if (FireworkPosition.y <= Renderer.GetHeight() / 2)
                 {
                     isExploded = true;
                     CreateParticles();
+                    UpdateCenterPosition();
                 }
             }
             else
             {
-                DrawFirework(); 
+                DrawFirework();
+                Thread.Sleep(50);
             }
         }
+
 
         
         public void DrawFirework()
@@ -104,22 +120,30 @@ namespace Cpsc370Final
 
         private void CreateParticles()
         {
-            for (int i = 0; i < 12; i++) // Create 12 particles
-            {
-                double angle = 2 * Math.PI * i / 12;
-                int offsetX = (int)(Math.Cos(angle) * 3); // Radius of 3
-                int offsetY = (int)(Math.Sin(angle) * 3);
+            particles.Clear();
+    
+            int radius = 5;
+            double particleDensity = 12;
 
+            for (int i = 0; i < particleDensity; i++)
+            {
+                double angle = 2 * Math.PI * i / particleDensity;
+                int offsetX = (int)Math.Round(Math.Cos(angle) * radius);
+                int offsetY = (int)Math.Round(Math.Sin(angle) * radius);
+        
                 var particle = new Particle
                 {
-                    particlePosition = new Position(FireworkPosition.x + offsetX, FireworkPosition.y + offsetY),
-                    particleSymbol = 'o',
-                    particleColor = this.particleColor
+                    particlePosition = new Position(
+                        FireworkPosition.x + offsetX,
+                        FireworkPosition.y + offsetY
+                    ),
+                    particleSymbol = 'o'
                 };
 
                 particles.Add(particle);
             }
         }
+
 
         public void Explode()
         {
