d2c17bf78a4b9d3da5eb45d767e4e50c1eaee0e3
BrianCassriel <bccassriel@gmail.com>
2025-01-30 16:48:56 -0800

    100% Coverage on RendererTest & VelocityTest

--- Cpsc370Final/IConsoleCanvas.cs
+++ Cpsc370Final/IConsoleCanvas.cs
@@ -3,9 +3,12 @@ namespace Cpsc370Final;
 public interface IConsoleCanvas
 {
     void Set(int x, int y, char symbol, ConsoleColor color);
+    void Text(int x, int y, string text, bool centered = false, ConsoleColor? foreground = null, ConsoleColor? background = null);
+    ConsoleRenderer.Pixel Get(int x, int y, bool backBuffer = true);
     void Clear();
     void Render();
     void CreateBorder();
+    void Resize(int width, int height);
     int Width { get; }
     int Height { get; }
 }
